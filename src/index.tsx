e34ec4797d636632799893d68987ea5bb698db4b
p83257961-wq <p83257961@gmail.com>
2026-05-01 15:48:37 +0800

    Add password gate to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import PasswordGate from "./PasswordGate";
 
 const rootElement = document.getElementById("root")!;
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <PasswordGate>
+      <App />
+    </PasswordGate>
   </React.StrictMode>
 );
